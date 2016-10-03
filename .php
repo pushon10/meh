dfea16959b4281770d32628f133dda2880b2b1f0
1404886 <z1pper89>
2016-10-03 14:52:16 +0100

    hmm

---
+++ .php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1404886
+ * Date: 03/10/2016
+ * Time: 13:49
+ */
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+<p>
+
+    <?php
+
+    $provisionedActivities = array("specs", "drugs", "Rock and Roll"); // declares the array
+
+    foreach($provisionedActivity as $x) {
+
+        print "<p>$x</p>";
+
+    }
+    ?>
+
+</p>
+
+</body>
+
+</html>
