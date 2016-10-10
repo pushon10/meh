8db974faddaa7a0cb540b3a64f56fbc4bf2b2c3e
1404886 <z1pper89>
2016-10-10 15:53:48 +0100

    databsetest

---
+++ Week2/after2010.php
@@ -0,0 +1,20 @@
+<?php
+
+include ("DBCONNECT1.php");
+
+if($db->connect_errno){
+    die('Connectfailed['.$db->connect_error.']');
+}
+
+$sql = "Select * from marvelmovies
+        Where yearReleased = '<=2010'";
+
+$result = $db -> query($sql);
+
+while($row = $result->fetch_array()){
+    echo "<p><strong>{$row[title]}</strong>: {$row[yearReleased]}</p>";
+}
+
+$result->close();
+
+?>
