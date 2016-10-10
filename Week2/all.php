e4569cf0736115fe9881f3020d8711c3a3f86b16
1404886 <z1pper89>
2016-10-10 15:50:15 +0100

    databsetest

---
+++ Week2/all.php
@@ -0,0 +1,19 @@
+<?php
+
+include ("DBCONNECT1.php");
+
+if($db->connect_errno){
+    die('Connectfailed['.$db->connect_error.']');
+}
+
+$sql = "Select * from marvelmovies;"
+
+$result = $db -> query($sql);
+
+while($row = $result->fetch_array()){
+    echo "<p><strong>{$row[title]}</strong>: {$row[yearReleased]}</p>";
+}
+
+$result->close();
+
+?>
