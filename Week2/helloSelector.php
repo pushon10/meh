7c71fcb51eeca51633edee4cefa0853cb4a1fe50
1404886 <z1pper89>
2016-10-17 14:26:31 +0100

    databsetest

--- Week2/helloSelector.php
+++ Week2/helloSelector.php
@@ -3,10 +3,10 @@
 "<p> Where would you like to say hello? "</p>;
 
 
-<a href="helloSelector.php?planet=earth">Earth</a>
+<a href="helloPrinter.php?planet=earth">Earth</a>
 
-<a href="helloSelector.php?planet=mars">Mars</a>
+<a href="helloPrinter.php?planet=mars">Mars</a>
 
-<a href="helloSelector.php?planet=uranus">Uranus</a>
+<a href="helloPrinter.php?planet=uranus">Uranus</a>
 
 
