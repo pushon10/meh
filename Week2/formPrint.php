e0dad1b352316c9c13a0e2e8c712aafdc81aa791
1404886 <z1pper89>
2016-10-17 14:36:16 +0100

    databsetest

---
+++ Week2/formPrint.php
@@ -0,0 +1,10 @@
+<html>
+
+
+<form action="formResponse.php" method="post">
+    <label>Forename</label><input type=“text” name=“forename”>
+    <label>Surname</label><input type=“text” name=“surname”>
+    <input type="submit" value="Submit">
+</form>
+
+</html>
---
+++ Week2/formResponse.php
@@ -0,0 +1,5 @@
+<?php
+$forename = $_POST[“forename”];
+$surname = $_POST[“surname”];
+echo $forename . $surname;
+?> 
