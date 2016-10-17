a0113a6a9bad740715a19094d5fd4c1ef8218df6
1404886 <z1pper89>
2016-10-17 14:40:57 +0100

    databsetest

--- Week2/formPrint.php
+++ Week2/formPrint.php
@@ -4,8 +4,8 @@
 <form action="formResponse.php" method="post">
     <label>Forename</label><input type=“text” name=“forename”>
     <label>Surname</label><input type=“text” name=“surname”>
-    <label>Forename</label><input type=“text” name=“gender”>
-    <label>Surname</label><input type=“text” name=“power”>
+    <label>Gender</label><input type=“text” name=“gender”>
+    <label>Power</label><input type=“text” name=“power”>
     <input type="submit" value="Submit">
 </form>
 
